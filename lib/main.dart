0a3ce3afc64bcf84961c820101c74a94d4fc1d6d
asylzhan <assylzhan.mukhambet@narxoz.kz>
2026-09-08 23:52:29 +0500

    task4

--- lib/main.dart
+++ lib/main.dart
@@ -75,3 +75,23 @@
 //   }
 //   print(count);
 // }
+// TASK4
+// void main() {
+//   List<int> numbers = [14, 88, 3, 42, 99, 12, 67];
+//
+//   int min = numbers[0];
+//   int max = numbers[0];
+//
+//   for (int i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > max) {
+//       max = numbers[i];
+//     }
+//
+//     if (numbers[i] < min) {
+//       min = numbers[i];
+//     }
+//   }
+//
+//   print("Max: $max");
+//   print("Min: $min");
+// }
