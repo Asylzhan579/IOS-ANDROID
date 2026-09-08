4fb77e86f7dfaa6bb59d20eace59d998044dfbea
asylzhan <assylzhan.mukhambet@narxoz.kz>
2026-09-08 23:40:29 +0500

    task3

--- lib/main.dart
+++ lib/main.dart
@@ -59,3 +59,19 @@
 //     );
 //   }
 // }
+// TASK 3
+// Vowel Counter
+// void main() {
+//   String text = "flutter mobile development";
+//   int count = 0;
+//   for (int i = 0; i < text.length; i++) {
+//     if (text[i] == 'a' ||
+//         text[i] == 'e' ||
+//         text[i] == 'i' ||
+//         text[i] == 'o' ||
+//         text[i] == 'u') {
+//       count++;
+//     }
+//   }
+//   print(count);
+// }
