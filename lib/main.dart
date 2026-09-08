88244e2e735fb52719c34c28fa8eb2f2c2b7caf9
asylzhan <assylzhan.mukhambet@narxoz.kz>
2026-09-08 23:25:17 +0500

    task2

--- lib/main.dart
+++ lib/main.dart
@@ -8,3 +8,54 @@
 //     }
 //   }
 // }
+// TASK 2
+// NEXT DAY
+
+// void main() {
+//   int day = 28;
+//   int month = 2;
+//   int year = 2024;
+//
+//   bool leapYear =
+//       year % 400 == 0 ||
+//           (year % 4 == 0 && year % 100 != 0);
+//
+//   int maxDay;
+//
+//   if (month == 2) {
+//     maxDay = leapYear ? 29 : 28;
+//   } else if (month == 4 ||
+//       month == 6 ||
+//       month == 9 ||
+//       month == 11) {
+//     maxDay = 30;
+//   } else {
+//     maxDay = 31;
+//   }
+//
+//   if (month < 1 ||
+//       month > 12 ||
+//       day < 1 ||
+//       day > maxDay) {
+//     print("Invalid date");
+//   } else {
+//     day++;
+//
+//     if (day > maxDay) {
+//       day = 1;
+//       month++;
+//     }
+//
+//     if (month > 12) {
+//       month = 1;
+//       year++;
+//     }
+//
+//
+//     print(
+//       "${day.toString().padLeft(2, '0')}."
+//           "${month.toString().padLeft(2, '0')}."
+//           "$year",
+//     );
+//   }
+// }
